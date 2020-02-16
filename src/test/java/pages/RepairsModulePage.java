ca76b754eb32f7237d07da73199c7aa05ca99aa3
delirish <delitrion5694>
2020-02-16 17:37:47 -0600

    iryna

--- src/test/java/pages/RepairsModulePage.java
+++ src/test/java/pages/RepairsModulePage.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class RepairsModulePage {
 
     public RepairsModulePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    //Iryna's elements:----------------------------------------------------------
     @FindBy(xpath = "//span[@title='Advanced Search...']")
     public WebElement advancedSearchButton;
 
@@ -28,5 +31,10 @@ public class RepairsModulePage {
 
     @FindBy(xpath = "(//li[@data-index='0'])[1]")
     public WebElement quotationsFilterSelectionCheckMark;
+
+    @FindBy(xpath = "//div//table//tbody//tr//td[@class='o_data_cell o_readonly_modifier']")
+    public List<WebElement> originalOrderList;
+
+    //-----------------------------------------------------------------------------------
 
  }
